b7450f8f48e818a86701d73c3c0f5b7377acd50c
aibo chang <aibochang@163.com>
2022-12-03 21:50:09 +0800

    初始化租房网站项目

---
+++ rental-project/src/router/index.ts
@@ -0,0 +1,12 @@
+import { createWebHistory } from 'vue-router'
+import HomeVue from '../views/Home/index.vue'
+import MineVue from '../views/Mine/index.vue'
+
+const routes = [
+  {
+    
+  }
+];
+
+
+export default routes
---
+++ rental-project/src/views/Home/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    hello,主页
+  </div>
+</template>
+  
+<script setup lang='ts'>
+  
+</script>
+  
+<style>
+  
+</style>
